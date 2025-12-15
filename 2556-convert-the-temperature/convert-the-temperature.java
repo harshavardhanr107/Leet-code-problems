8e749caa4472237a53f9ebf62265dd68a2131e78
harshavardhanr107 <harshavardhan.r107@gmail.com>
2025-12-15 09:43:25 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (61.99%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.java
+++ 2556-convert-the-temperature/convert-the-temperature.java
@@ -3,9 +3,6 @@ class Solution {
         
             double kel = celsius+273.15;
             double fahren = celsius*1.80+32.00;
-            double[] result = new double[2];
-            result[0] = kel;
-            result[1] = fahren;
-            return result;
+            return new double[]{kel,fahren};
     }
 }
